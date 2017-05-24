8925296652e344940c4fd58e936884186dbb3716
jerodepa <jeroen.depauw1@student.arteveldehs.be>
2017-05-24 19:10:17 +0200

    fix navigation

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// responsive nav
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav__nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav__nav";
+    }
+}
