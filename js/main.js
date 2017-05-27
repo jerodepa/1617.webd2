63a5c4aec3a0c561bdae4695f5663ee68ed593ce
jerodepa <jeroen.depauw1@student.arteveldehs.be>
2017-05-27 20:54:49 +0200

    yessss

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 // responsive nav
+var button = document.querySelector(".hamburger"); 
+var nav = document.querySelectorAll(".nav__grid__column-5"); 
+var count = 0; button.addEventListener("click", openAndClose); 
+
+function openAndClose() { 
+    if (count % 2 < 1) { 
+        button.src = "../assets/icons/escape.png"; 
+        for (i = 0; i < nav.length; ++i) { 
+            nav[i].style.display = "block"; 
+        } count++; 
+    } 
+    else { 
+        button.src = "../assets/icons/bars.png"; 
+        for (i = 0; i < nav.length; ++i) { 
+            nav[i].style.display = "none"; } count++; 
+        } 
+    }
+
 
 
 // kleur bar veranderd bij scroll
